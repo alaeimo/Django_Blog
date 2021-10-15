9ecd46ce4b2062c282bcaac8aa7167503c229fff
Mohammad Alaei <alaeimono@gmail.com>
2021-10-15 22:29:30 +0330

    Add Post class to the models and create database and make migrations.

--- main/views.py
+++ main/views.py
@@ -1,19 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {'author':'Mohammad',
-     'title': 'Title of Post 01',
-     'content':'This is the content of post 01',
-     'post_date':'January 27, 2021',
-     'avatar':'#e83e8c'},
-     {'author':'Ali',
-     'title': 'Title of Post 02',
-     'content':'This is the content of post 02',
-     'post_date':'January 25, 2021',
-     'avatar':'#6f42c1'},
-]
 def home(request):
-    context = {'posts':posts}
+    context = {'posts':Post.objects.all()}
     return render(request, 'index.html', context=context)
 
 def about(request):
