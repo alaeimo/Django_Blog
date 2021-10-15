b9b550ccfc67c246b26f0e04c5f89a52b5667d87
Mohammad Alaei <alaeimono@gmail.com>
2021-10-15 15:06:51 +0330

    Create bootstrap templates for layout, index and about pages.

--- main/views.py
+++ main/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {'author':'Mohammad',
+     'title': 'Title of Post 01',
+     'content':'This is the content of post 01',
+     'post_date':'January 27, 2021',
+     'avatar':'#e83e8c'},
+     {'author':'Ali',
+     'title': 'Title of Post 02',
+     'content':'This is the content of post 02',
+     'post_date':'January 25, 2021',
+     'avatar':'#6f42c1'},
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {'posts':posts}
+    return render(request, 'index.html', context=context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About </h1>')
+    return render(request, 'about.html', {'title':'About'})
