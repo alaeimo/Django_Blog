d28d716125a56037dd262096db009d6fab4d3a4b
Mohammad Alaei <alaeimono@gmail.com>
2021-10-17 15:53:52 +0330

    Create and modify register pages backend and url.  We used django-crispy-form to make register form a little prettier.

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main.apps.MainConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+def register(request): 
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'New account created for {username}')
+            return redirect('home') 
+    else:
+        form = UserRegisterForm(request.POST)
+
+    return render(request, 'users/register.html',{'form': form ,'title':'Register'})
+
+def login(request):
+    return render(request, 'users/login.html',{'title':'Login'})
