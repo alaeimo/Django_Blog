aabc9900a627201e27b7039c951bdc8993de8a18
Mohammad Alaei <alaeimono@gmail.com>
2021-10-18 18:25:58 +0330

    Add user login, logout and profile pages. Now users can log in or log out or go to their profiles.

--- users/views.py
+++ users/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
-
 def register(request): 
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request,f'New account created for {username}')
-            return redirect('home') 
+            messages.success(request,f'New account created for {username}! You can login now!')
+            return redirect('login') 
     else:
         form = UserRegisterForm(request.POST)
 
     return render(request, 'users/register.html',{'form': form ,'title':'Register'})
 
-def login(request):
-    return render(request, 'users/login.html',{'title':'Login'})
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html',{'title':'profile'})
