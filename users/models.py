9dd0e681a49399370179d2ddb174e7da4c981952
Mohammad Alaei <alaeimono@gmail.com>
2021-10-23 10:22:45 +0330

    Edit user/models.py

--- users/models.py
+++ users/models.py
@@ -8,8 +8,8 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-    def save(self):
-        super().save()
+    def save(self,*args,**kwargs):
+        super().save(*args,**kwargs)
         img = Image.open(self.image.path)
         if img.height >300 or img.width>300:
             output_size = (300,300)
