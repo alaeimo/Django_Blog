b3705d58cfa3e6417eade6a2a36125a90f25beb1
Mohammad Alaei <alaeimono@gmail.com>
2021-10-24 12:25:17 +0330

    Edit DEBUG and SECRET_KEY in settings.

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fls(ul#2^$*s22^ls($u&5*-sc!m&2le$$mvzrtic!3t%nc7*='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
